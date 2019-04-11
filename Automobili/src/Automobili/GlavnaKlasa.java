d21ff31f1960a49057b66c7fb7902ef64b40146b
oik <oik@LAPTOP-MM2909R6>
2019-04-11 20:03:36 +0200

    Dodla sam metodu za unos marke

--- Automobili/src/Automobili/GlavnaKlasa.java
+++ Automobili/src/Automobili/GlavnaKlasa.java
@@ -4,6 +4,10 @@ public class GlavnaKlasa {
 
 	public static void main(String[] args) {
 		
+		Metode metode= new Metode();
+		metode.ubaciMarku("Fiat", "Italija");
+		
+		
 
 	}
 
--- Automobili/src/Automobili/Metode.java
+++ Automobili/src/Automobili/Metode.java
@@ -2,19 +2,53 @@ package Automobili;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Metode {
 
 	final Connection konektujSe(String baza) throws SQLException {
 	
-		final String url="jdbc:mysql://localhost:3306/" + baza + "?useSSL=false";
+		final String url="jdbc:mysql://localhost:3306/" + baza + "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 		final String userName="root";
 		final String password="root";
 	
 	
 	return DriverManager.getConnection(url, userName, password);
 	
+	}	
+	
+	public void ubaciMarku(String nazivMarke, String zemlja) {
+		
+	Connection konekcija= null;
+	PreparedStatement pst= null;
+	
+	try {
+		konekcija=konektujSe("automobili");
+		String query = "INSERT INTO marke VALUES (?,null,?)";
+		pst= konekcija.prepareStatement(query);
+		pst.setString(1, nazivMarke);
+		pst.setString(2, zemlja);
+		pst.execute();
+		System.out.println("Uspesan unos");
+	} catch (SQLException e) {
+		System.out.println("Neuspesan unos");
+		e.printStackTrace();
+	}finally {
+		try {
+			pst.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			konekcija.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 	
 	}	
 }
