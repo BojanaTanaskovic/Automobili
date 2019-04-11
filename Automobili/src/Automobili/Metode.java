c94020a35167e658555b532342ae655f4d7046f0
oik <oik@LAPTOP-MM2909R6>
2019-04-11 19:18:07 +0200

    Ovo je nas prvi komit

---
+++ Automobili/src/Automobili/Metode.java
@@ -0,0 +1,20 @@
+package Automobili;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Metode {
+
+	final Connection konektujSe(String baza) throws SQLException {
+	
+		final String url="jdbc:mysql://localhost:3306/" + baza + "?useSSL=false";
+		final String userName="root";
+		final String password="root";
+	
+	
+	return DriverManager.getConnection(url, userName, password);
+	
+	
+	}	
+}
